14f6801bb7a676cc0560d517ef348df953f34a95
anurag33 <45606134+anurag33@users.noreply.github.com>
2023-03-18 17:15:32 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('10.108.55.77','root','rootpassword','tms_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('10.104.232.54','root','rootpassword','tms_db')or die("Could not connect to mysql".mysqli_error($con));
